0d2d6b2e76585fac1344b6bc54ec92326915d5d8
Gabriel Queiroz <gabriel-cso@hotmail.com>
2022-09-10 22:31:59 -0300

    add swagger documentation

--- catalog-consumer/src/main/java/br/com/queiroz/catalogconsumer/spring/ConsumerApplication.java
+++ catalog-consumer/src/main/java/br/com/queiroz/catalogconsumer/spring/ConsumerApplication.java
@@ -2,8 +2,10 @@ package br.com.queiroz.catalogconsumer.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ConsumerApplication {
   public static void main(String[] args) {
     SpringApplication.run(ConsumerApplication.class, args);
--- catalog-financial/src/main/java/br/com/queiroz/catalogfinancial/FinancialApplication.java
+++ catalog-financial/src/main/java/br/com/queiroz/catalogfinancial/FinancialApplication.java
@@ -2,8 +2,10 @@ package br.com.queiroz.catalogfinancial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class FinancialApplication {
   public static void main(String[] args) {
     SpringApplication.run(FinancialApplication.class, args);
